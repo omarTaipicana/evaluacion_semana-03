ddb8950f6657b2486c2ea1a62f5946517d11258a
ferTaipi <fernandoparadoja@gmail.com>
2022-09-12 19:23:58 -0500

    color

--- src/componentes/Residents.jsx
+++ src/componentes/Residents.jsx
@@ -9,21 +9,32 @@ const Residents = ({ resident }) => {
         axios.get(resident)
             .then(res => setListResident(res.data))
     }, [])
-
-    // console.log(listResident)
+    const colorCircle = () => {
+        if (listResident.status === "Alive") {
+            return "green"
+        } else {
+            if (listResident.status === "Dead") {
+                return "red"
+            } else {
+                return "black"
+            }
+        }
+    }
+    console.log(colorCircle())
+    // console.log(listResident.status)
 
     return (
-            <li className='card'>
-                <img className='card-img' src={listResident.image} alt="" />
-                <div>
-                    <h3>{listResident.name}</h3>
-                    <h4>{listResident.status}</h4>
-                    <p className='info-resident'>origen</p>
-                    <h4>{listResident.origin?.name}</h4>
-                    <p className='info-resident'>episodes where appear</p>
-                    <h4>{listResident.episode?.length}</h4>
-                </div>
-            </li>
+        <li className='card'>
+            <img className='card-img' src={listResident.image} alt="" />
+            <div>
+                <h3>{listResident.name}</h3>
+                <h4>{listResident.status}{" "}<i className="fa fa-circle" style={{ color: colorCircle() }}></i>  </h4>
+                <p className='info-resident'>origen</p>
+                <h4>{listResident.origin?.name}</h4>
+                <p className='info-resident'>episodes where appear</p>
+                <h4>{listResident.episode?.length}</h4>
+            </div>
+        </li>
     );
 };
 
